4f7acaecc244063090282f550589ecce93f834c2
veronicajn <veronicajn230285@gmail.com>
2022-01-30 21:25:41 +0100

    first commit

--- src/Components/home/home.jsx
+++ src/Components/home/home.jsx
@@ -3,14 +3,16 @@ import './home.scss';
 
 const Home = () => {
     return (
-       
+       <div className="container_principal">
+
+      
             <div className="container_home">
                 <p className="name">Coral Torralba Núñez</p>
                 <p className="description">Junior Mobile Developer</p>
                 <p className="message">Investigación, creación, diseño, definición, monitorización y solución.</p>
             
             </div>
-            
+        </div>     
         
 
     )
